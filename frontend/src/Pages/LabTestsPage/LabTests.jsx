baf59dc7e1f97c853f1fb9998a01037be87844bb
ahsanofficial <bega95091@gmail.com>
2022-11-11 00:48:48 +0530

    update labtests

--- frontend/src/Pages/LabTestsPage/LabTests.jsx
+++ frontend/src/Pages/LabTestsPage/LabTests.jsx
@@ -4,7 +4,9 @@ const LabTests = () => {
     return (
         <TimeStyled>
             <div id='main'>
-
+                <div>
+                    <img src="https://onemg.gumlet.io/assets/9aaa4ea0-dcf1-11ec-a049-0273d826c612.png?format=auto" alt="img" />
+                </div>
                 <div>
                     <div id='lab'>
                         <span>Lab Test From The Comfort Of Your Home</span>
@@ -14,7 +16,6 @@ const LabTests = () => {
                         <div>
                             <img src="https://onemg.gumlet.io/Safe_38x38_labs_oe5ieq.png?format=auto" alt="img" />
                             <p>100% Safe & Hygienic</p>
-
                         </div>
                         <div>
                             <img src="https://onemg.gumlet.io/new_color_images/Lab_delivery_2x.png?format=auto" alt="img" />
@@ -33,10 +34,33 @@ const LabTests = () => {
                             <p>Best Prices Guaranteed</p>
                         </div>
                     </div>
-                    <button id="package">VIEW POPULAR PACKAGES</button>
+                    <div id="package">
+                        <button>VIEW POPULAR PACKAGES</button>
+                    </div>
                 </div>
             </div>
-        </TimeStyled>
+            <div id="popular">
+                <h3>Popular Tests</h3>
+                <div>
+                    <div>
+                        <div>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="18" cy="18" r="18" fill="#FFFCF8" stroke="#FFFFFF"></circle><g stroke="#000" stroke-linecap="round" stroke-linejoin="round"><path fill="#FFF" d="M10.945 17.814v7.067c0 .721.574 1.309 1.284 1.309h4.11v-5.23h3.258v5.23h4.124c.71 0 1.286-.588 1.286-1.309v-7.067m1.943 0L17.975 9.05 9 17.814"></path><path d="M25.146 13.268v-1.655c0-.722-.574-1.309-1.285-1.309H22.35"></path></g></g></svg>
+                        </div>
+                        <div>
+                            <h5>HEALTH TEST AT YOUR HOME</h5>
+                        </div>
+                        <div>
+                            <div><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><g fill="#212121" fill-rule="evenodd"><path d="M17.5 9.017h.01-.01zM8.999 1C4.595 1 1.008 4.579 1 8.985.991 13.396 4.573 16.993 8.984 17H9c4.403 0 7.991-3.578 8-7.984.008-4.411-3.574-8.007-7.985-8.016h-.016zM9 18h-.018C4.02 17.991-.01 13.946 0 8.983.009 4.026 4.045 0 8.999 0h.018C13.979.01 18.01 4.056 18 9.018 17.989 13.974 13.954 18 9 18z"></path><path d="M9.527 8.639V4.946a.446.446 0 0 0-.892 0v3.692l-3.688-.004a.446.446 0 0 0 0 .892l3.688.003v3.686a.446.446 0 0 0 .892 0V9.53l3.752.004a.446.446 0 0 0 0-.892L9.527 8.64z"></path></g></svg></div>
+                            <div>COVID-RTPCR</div>
+                        </div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="38" height="38" viewBox="0 0 38 38"><g fill="none" fill-rule="evenodd" transform="translate(1 1)"><circle cx="18" cy="18" r="18" fill="#FFFCF8" stroke="#FFFFFF"></circle><g stroke="#000"><path stroke-linecap="round" d="M8.475 21.675h11.723"></path><path fill="#FFF" stroke-linejoin="round" d="M8.743 18.932h5.724a2.4 2.4 0 0 1 2.4 2.4c0 .19-.154.343-.343.343H8.4v-2.4c0-.19.153-.343.343-.343z"></path><path fill="#FFF" stroke-linejoin="round" d="M19.468 20.303a1.371 1.371 0 1 1-2.743 0 1.371 1.371 0 0 1 2.743 0z"></path><path fill="#FFF" stroke-linejoin="round" d="M18 8.4a9.604 9.604 0 0 0-9.062 6.422h3.94a6.027 6.027 0 1 1 .347 6.858L8.4 21.677v1.83h1.74A9.586 9.586 0 0 0 18 27.6c5.302 0 9.6-4.298 9.6-9.6S23.302 8.4 18 8.4z"></path></g></g></svg>
+                        <h5>VISIT A LAB NEAR YOU</h5>
+                    </div>
+                </div>
+            </div>
+        </TimeStyled >
     )
 }
 export { LabTests }
@@ -84,14 +108,32 @@ box-sizing: border-box;
     width: 100%;
 }
 #package{
+    text-align: left;
+}
+#package>button{
     padding: 10px 36px;
-    margin:20px 0;
     background-color: rgb(255,111,97);
     border: none;
     color: white;
     font-weight: bold;
-    text-align: left;
     border-radius: 5px;
-    margin-left: 0;
+}x
+#popular{
+    padding-top: 20px;
+    padding-left: 52px;
+    margin: auto;
+}
+#popular>div{
+    display: flex;
+    border: 1px solid red;
+    width: 40%;
+    margin: auto;
+}
+#popular>div>div{
+    display: flex;
+    border: 1px solid red;
+}
+#popular>h3{
+    text-align: left;
 }
 `
