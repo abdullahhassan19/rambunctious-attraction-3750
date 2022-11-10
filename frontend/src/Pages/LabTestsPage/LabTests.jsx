77a4f68bedfe84d8ad6adae462cc3596027f76d6
ahsanofficial <bega95091@gmail.com>
2022-11-10 12:08:20 +0530

    labTests update

--- frontend/src/Pages/LabTestsPage/LabTests.jsx
+++ frontend/src/Pages/LabTestsPage/LabTests.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+// import styled from 'styled-components'
 
-function LabTests() {
+const LabTests = () => {
     return (
-        <div>LabTests</div>
+        // <TimeStyled>
+        <div id='main'>
+            <div></div>
+            <div>
+                <h1>Lab Test From The Comfort Of Your Home</h1>
+                <p>50,00,000+ lab tests booked | 20,00,000+ satisfied customers</p>
+            </div>
+        </div>
+        // </TimeStyled>
     )
 }
+export { LabTests }
 
-export default LabTests
+// const TimeStyled = styled.div`
+// padding-left: 20px;
+// padding-right: 20px;
+// height:100%;
+// width: 100%;
+// box-sizing: border-box;
+// #main{
+//     display:flex;
+// }
+// `
